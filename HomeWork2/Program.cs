4e50c7a22574a8e0134a727b0e9c9abd14962d69
Mukhtar Konyrtayev <konyrtaevm@gmail.com>
2023-02-20 21:45:00 +0600

    HW2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа: "+stringNumber[1]);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("Третья цифра: " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Задача 15 ");
+
+Console.Write("Введи цифру, обозначающую день недели(от 1 до 7): ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("Этот день выходной: Да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Это вообще не день недели");
+  }
+  else Console.WriteLine("Этот день выходной: Нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
